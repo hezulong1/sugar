c030644395c7259c0adcc2906f0add79c4797ad7
hezulong1 <626993211.com>
2023-12-14 17:31:18 +0800

    init commit

---
+++ packages/components-vue/close-button/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Close from '../icon-close/index.vue'
+</script>
+
+<template>
+  <button class="close-button" type="button" aria-label="Close">
+    <Close />
+  </button>
+</template>
